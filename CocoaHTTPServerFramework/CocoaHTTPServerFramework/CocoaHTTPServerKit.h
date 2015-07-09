d9e8443855ad94dc8fa33cc20de6bf0b885329f0
quellish <github@quellish.neomailbox.com>
2015-07-08 19:44:35 -0700

    Initial import

---
+++ CocoaHTTPServerFramework/CocoaHTTPServerFramework/CocoaHTTPServerKit.h
@@ -0,0 +1,18 @@
+//
+//  CocoaHTTPServerFramework.h
+//  CocoaHTTPServerFramework
+//
+//  Created by Dan Zinngrabe on 7/8/15.
+//  Copyright (c) 2015 Dan Zinngrabe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <CocoaHTTPServerKit/HTTPServer.h>
+#import <CocoaHTTPServerKit/HTTPConnection.h>
+#import <CocoaHTTPServerKit/HTTPLogging.h>
+#import <CocoaHTTPServerKit/HTTPMessage.h>
+#import <CocoaHTTPServerKit/HTTPResponse.h>
+#import <CocoaHTTPServerKit/HTTPAuthenticationRequest.h>
+
+
